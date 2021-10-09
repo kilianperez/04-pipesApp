e30540386a2b37f120caa942748336d03d171bb8
Kilian Perez <kilianperez92@gmail.com>
2021-10-09 14:07:20 +0200

    Cambiar el idioma por defecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -6,7 +6,10 @@ import { SharedModule } from './shared/shared.module';
 import { AppRouterModule } from './app-router.module';
 import { VentasModule } from './ventas/ventas.module';
 
-
+// Cambiar el locale de la app (idioma)
+import  localeEs  from "@angular/common/locales/es";
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(locaes);
 
 @NgModule({
   declarations: [
@@ -18,7 +21,8 @@ import { VentasModule } from './ventas/ventas.module';
     AppRouterModule,
     VentasModule
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue:'es'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
