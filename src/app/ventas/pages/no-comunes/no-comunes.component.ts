942d012d091ea55b0cd1415665125a62517e49b0
Kilian Perez <kilianperez92@gmail.com>
2021-10-12 14:33:00 +0200

    I18nPluralPipe

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -3,15 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-no-comunes',
   templateUrl: './no-comunes.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class NoComunesComponent {
-
+  // i18nSelect
   nombre: string = 'Kilian';
   genero: string = 'masculino';
   invitacionMapa = {
-    'masculino': 'invitalo',
-    'femenino': 'invitarla'
-  }
+    masculino: 'invitalo',
+    femenino: 'invitarla',
+  };
+
+  // i18nSPlural
+  clientes: string[] = ['Maria', 'Pedro', 'Juan','Eduardo', 'Fernando', 'Raquel'];
+  clientesMapa = {
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    '=2': 'tenemos 2 clientes esperando.',
+    'other': 'tenemos # clientes esperando.'
+  };
 }
