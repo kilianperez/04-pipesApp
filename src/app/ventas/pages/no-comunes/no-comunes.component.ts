06e5b61ed81914441dbf764eeb695ce41315e563
Kilian Perez <kilianperez92@gmail.com>
2021-10-12 14:23:01 +0200

    I18nSelectPipe

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-no-comunes',
@@ -6,11 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class NoComunesComponent implements OnInit {
+export class NoComunesComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  nombre: string = 'Kilian';
+  genero: string = 'masculino';
+  invitacionMapa = {
+    'masculino': 'invitalo',
+    'femenino': 'invitarla'
   }
-
 }
