8fba5b6c5f55b968d84552748ea394b364fbb1e7
Kilian Perez <kilianperez92@gmail.com>
2021-10-13 12:37:33 +0200

    Ordenar héroes por nombre - OrdenarPipe

--- src/app/ventas/ventas.module.ts
+++ src/app/ventas/ventas.module.ts
@@ -10,6 +10,7 @@ import { BasicosComponent } from './pages/basicos/basicos.component';
 import { OrdenarComponent } from './pages/ordenar/ordenar.component';
 import { MayusculasPipe } from './pipes/mayusculas.pipe';
 import { VuelaPipe } from './pipes/vuela.pipe';
+import { OrdenarPipe } from './pipes/ordenar.pipe';
 
 
 
@@ -21,7 +22,8 @@ import { VuelaPipe } from './pipes/vuela.pipe';
     BasicosComponent,
     OrdenarComponent,
     MayusculasPipe,
-    VuelaPipe
+    VuelaPipe,
+    OrdenarPipe
   ],
   exports:[
     NumerosComponent,
